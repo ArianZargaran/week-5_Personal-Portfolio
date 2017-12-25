1337139f6c73f8c46570d943b4e86c4392daaca3
ArianZargaran <arianzf@gmail.com>
2017-12-24 21:18:05 -0800

    solving Google Maps issue

--- js/maps.js
+++
@@ -1,9 +0,0 @@
-function initialize(){
-  var mapProp = {
-    center: new google.maps.LatLng(37.3378741,-121.9340505),
-    zoom: 19,
-    mapTypeId: google.maps.MapTypeId.ROADMAP 
-  }
-  var map = new google.maps.Map(document.getElementById('google-map'), mapProp)
-}
-google.maps.event.addDomListener(window, 'load', initialize)
